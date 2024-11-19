fcc5b86805d87d29fefca77bf1e8a7d64cc9be3e
dayprksa <dappajayaperkasa@gmail.com>
2024-11-19 13:41:09 +0700

    Day 2

---
+++ PHP/biodata.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "Daffa Jaya Perkasa";
+$alamat = "Indonesia, Jawa Barat, Kab. Bandung";
+$ttl = "27 Februari 2008";
+$email = "dappajayaperkasa@gmail.com";
+$hobi = "Bermain Game (Valorant), Menonton Film (Source Code), Membaca Buku (Filosofi Teras)";
+?>
